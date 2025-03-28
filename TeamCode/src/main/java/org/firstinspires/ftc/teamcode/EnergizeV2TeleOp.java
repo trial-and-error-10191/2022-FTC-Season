3ab96295c70967731e21320a7d2e8193c92a48f2
kahvi <kahvidsachi@gmail.com>
2025-03-27 20:57:15 -0500

    Updated Bessie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EnergizeV2TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EnergizeV2TeleOp.java
@@ -240,6 +240,8 @@ public class EnergizeV2TeleOp extends LinearOpMode {
             }
 
             // Shows the elapsed game time and wheel power.
+            telemetry.addData("Limit Switch", "%b", limitSwitch.getState());
+            telemetry.addData("Touch Sensor", "%b", sensorTouch.getState());
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
